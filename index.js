5fa7b0e32615ea1866d6ef4800287478b7e75008
Zack Enders <enders.zack@gmail.com>
2018-01-18 17:42:34 -0500

    Centered the sub button on modal. Fixed some css and html.

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 let modal = document.getElementById('modal')
 let close = document.getElementById('close')
+let modalOverlay = document.getElementById('modal-overlay')
 
 
 close.addEventListener("click", function() {
  	modal.style.display = "none"
+ 	modalOverlay.style.display = "none"
 });
 
 setTimeout(function(){
-	modal.style.display = "grid"
-	// close.style.display = "block"
-}, 4000)
+	modal.style.display = "block"
+	modalOverlay.style.display = "block"
+}, 3000)
 
 
